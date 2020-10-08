4de7db49e805fcde74aa0b3508aa9b82750e5a1c
anton <zas@see.ru>
2020-10-07 23:04:43 -0400

    11

---
+++ src/main/java/ru/job4j/io/Matches.java
@@ -0,0 +1,30 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        int spic = 11;
+        System.out.println("11 Спичек");
+        Scanner scanner = new Scanner(System.in);
+        while (spic != 0) {
+            System.out.println("Введите количество от 1 до 3:");
+            int num = scanner.nextInt();
+            if (num == 1) {
+                spic -= num;
+            } else if (num == 2) {
+                spic -= num;
+            } else if (num == 3) {
+                spic -= num;
+            } else {
+                System.out.println("Введите правильное число");
+            }
+            if (spic > 0) {
+                System.out.println("Осталось спичек: " + spic);
+            } else {
+                System.out.println("Вы выйграли");
+            }
+
+        }
+    }
+}

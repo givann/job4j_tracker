8956dc832bc1a0a7da6886df255f79675553414d
anton <zas@see.ru>
2020-10-08 22:27:37 -0400

    Upcast

---
+++ src/main/java/ru/job4j/oop/College.java
@@ -0,0 +1,10 @@
+package ru.job4j.oop;
+
+public class College {
+    public static void main(String[] args) {
+        Freshman freshman = (Freshman) new Student();
+        Freshman freshman1 = (Freshman) new Object();
+        Student student = (Student) new Object();
+
+    }
+}

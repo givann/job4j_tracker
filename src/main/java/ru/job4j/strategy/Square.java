cfe5e04d1124787dca404f3f46faaed3d2c60a62
anton <zas@see.ru>
2020-10-09 01:43:06 -0400

    ww

--- src/main/java/ru/job4j/strategy/Square.java
+++ src/main/java/ru/job4j/strategy/Square.java
@@ -12,7 +12,7 @@ public class Square implements Shape {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Square().draw());
+        System.out.println(new Square().draw()  );
     }
 }
 

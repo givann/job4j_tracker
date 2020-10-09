88b9df8b1b99e0f1c6833c54c21c205ef88818f5
anton <zas@see.ru>
2020-10-08 23:10:38 -0400

    newUI

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -1,44 +1,34 @@
 package ru.job4j.tracker;
 
-import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
-import java.util.Scanner;
+
 
 
 public class StartUI {
-    public void init(Scanner scanner, Tracker tracker) {
+    public void init(Input input, Tracker tracker) {
         boolean run = true;
         while (run) {
             this.showMenu();
-            System.out.print("Select: ");
-            int select = Integer.valueOf(scanner.nextLine());
+            int select = Integer.valueOf(input.askInt("Select: "));
             if (select == 0) {
                 System.out.println("=== Create a new Item ====");
-                System.out.print("Enter name: ");
-                String name = scanner.nextLine();
+                String name = input.askStr("Enter name: ");
                 Item item = new Item(name);
                 tracker.add(item);
             } else if (select == 1) {
                 System.out.println(Arrays.toString(tracker.findAll()));
             } else if (select == 2) {
-                System.out.println("Enter id:");
-                int id = scanner.nextInt();
-                scanner.nextLine();
-                System.out.println("Enter item:");
-                if (tracker.replace(id, new Item(scanner.nextLine()))) {
+                int id = input.askInt("Enter id:");
+                if (tracker.replace(id, new Item(input.askStr("Enter item:")))) {
                     System.out.println("Succesful");
                 } else System.out.println("Unseccesful");
             } else if (select == 3) {
-                System.out.println("Enter id");
-                int id = scanner.nextInt();
-                scanner.nextLine();
+                int id = input.askInt("Enter id");
                 if (tracker.delete(id)){
                     System.out.println("Succesful");
                 } else System.out.println("Unseccesful");
             } else if (select == 4) {
-                System.out.println("Enter id:");
-                int id = scanner.nextInt();
-                scanner.nextLine();
+                int id = input.askInt("Enter id:");
                 Item item = tracker.findById(id);
                 if (item.getId() != -1) {
                     System.out.println(item.getName());
@@ -46,9 +36,7 @@ public class StartUI {
                     System.out.println("Not exist");
                 }
             } else if (select == 5) {
-                System.out.println("Enter name: ");
-                String newName = scanner.next();
-                scanner.nextLine();
+                String newName = input.askStr("Enter name: ");
                 if (tracker.findByName(newName).length == 0) {
                     System.out.println("Not exist");
                 } else
@@ -73,8 +61,8 @@ public class StartUI {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
-        new StartUI().init(scanner, tracker);
+        new StartUI().init(input, tracker);
     }
 }

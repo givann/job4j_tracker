e201e518a0ff850f3af7e5008d8afcad3ca2e5e9
anton <zas@see.ru>
2020-10-09 21:40:38 -0400

    ww

--- src/main/java/ru/job4j/io/Matches.java
+++ src/main/java/ru/job4j/io/Matches.java
@@ -7,19 +7,36 @@ public class Matches {
         int spic = 11;
         System.out.println("11 Спичек");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name: ");
+        String nameOne = scanner.nextLine();
+        System.out.println("Enter name: ");
+        String nameTwo = scanner.nextLine();
+        int count = 0;
         while (spic != 0) {
             System.out.println("Введите количество от 1 до 3:");
             int num = scanner.nextInt();
             if (num == 1 || num == 2 || num == 3) {
                 spic -= num;
+                count++;
             } else {
                 System.out.println("Введите правильное число");
             }
-            if (spic > 0) {
-                System.out.println("Осталось спичек: " + spic);
-            } else {
-                System.out.println("Вы выйграли");
+            if (count % 2 != 0) {
+                if (spic > 0) {
+                    System.out.println("Осталось спичек: " + spic);
+                } else {
+                    System.out.println("Вы выйграли " + nameOne);
+                }
+
+            } else{
+                if (spic > 0) {
+                    System.out.println("Осталось спичек: " + spic);
+                } else {
+                    System.out.println("Вы выйграли " + nameTwo);
+                }
+
             }
+
 
         }
     }

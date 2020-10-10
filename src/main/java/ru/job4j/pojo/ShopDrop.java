6c8abc76ba89d2a6a68d10481bd9426a0bc098e7
anton <zas@see.ru>
2020-10-09 21:50:22 -0400

    ShDr

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -5,13 +5,9 @@ import ru.job4j.oop.Product;
 public class ShopDrop {
     public static Product[] leftShift(Product[] products, int index) {
         for (int i = index; i < products.length - 1; i++) {
-            products[products.length -1] = null;
-            products[i + 1] = null;
+            products[i] = products[i + 1];
         }
+        products[products.length - 1] = null;
         return products;
     }
 }
-
-
-// products[i] = products[i + 1];
-//
--- src/test/java/ru/job4j/pojo/ShopDropTest.java
+++ src/test/java/ru/job4j/pojo/ShopDropTest.java
@@ -15,16 +15,16 @@ public class ShopDropTest {
         products[0] = new Product("Milk", 10);
         products[1] = new Product("Bread", 4);
         Product[] rsl = ShopDrop.leftShift(products, 0);
-        assertThat(rsl[0].getName(), is("Milk"));
+        assertThat(rsl[0].getName(), is("Bread"));
         assertThat(rsl[1], is(nullValue()));
     }
 
     @Test
     public void whenDropLast() {
-       Product[] products = new Product[2];
+        Product[] products = new Product[2];
         products[0] = new Product("Milk", 10);
         products[1] = new Product("Bread", 4);
-        Product[] rsl = ShopDrop.leftShift(products, 0);
+        Product[] rsl = ShopDrop.leftShift(products, 1);
         assertThat(rsl[0].getName(), is("Milk"));
         assertThat(rsl[1], is(nullValue()));
     }

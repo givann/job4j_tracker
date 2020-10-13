c7f726d9b2a383e9201628a36f4100a887165bf5
anton <zas@see.ru>
2020-10-13 00:56:32 -0400

    Ex

--- src/main/java/ru/job4j/ex/Fact.java
+++ src/main/java/ru/job4j/ex/Fact.java
@@ -3,12 +3,12 @@ package ru.job4j.ex;
 public class Fact {
     public static void main(String[] args) {
         Fact fact = new Fact();
-        fact.calc(-1);
+        System.out.println(fact.calc(3));
     }
 
-    public int calc(int n) {
+    public static int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("N could not be less then 0");
         }
         int rsl = 1;
         for (int index = 1; index <= n; index++) {

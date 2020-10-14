43da8114f54939c8d0f3ed8d87fb1286245369f3
anton <zas@see.ru>
2020-10-13 21:41:29 -0400

    Ex

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -3,10 +3,11 @@ package ru.job4j.ex;
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        for (String s : value) {
-            if (s != key) {
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+            } else
                 throw new ElementNotFoundException();
-            }
         }
         return rsl;
     }
@@ -14,7 +15,7 @@ public class FindEl {
     public static void main(String[] args) {
         String[] strings = new String[0];
         try {
-            indexOf(strings,"rt");
+            indexOf(strings, "rt");
         } catch (ElementNotFoundException e) {
             e.printStackTrace();
         }

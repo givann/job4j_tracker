a7e3ce70daecd01b12142738279ef6c5c675d990
anton <zas@see.ru>
2020-10-24 00:16:21 -0400

    Al

---
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,58 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        users.putIfAbsent(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+        User user = findByPassport(passport);
+        List<Account> accounts = users.get(user);
+        if (!accounts.contains(account)) {
+            accounts.add(account);
+
+        }
+    }
+
+    public User findByPassport(String passport) {
+        for (User user : users.keySet()) {
+            if (user.getPassport().equals(passport)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        User user = findByPassport(passport);
+        List<Account> accounts = users.get(user);
+        int index = accounts.indexOf(new Account(requisite, -1));
+        if (index == -1) {
+            return null;
+        }
+        return accounts.get(index);
+
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        Account account = findByRequisite(srcPassport, srcRequisite);
+        Account account1 = findByRequisite(destPassport, destRequisite);
+        if (amount > account.getBalance() || findByRequisite(srcPassport, srcRequisite) == null) {
+            return rsl;
+        } else {
+            account.setBalance(account.getBalance() - amount);
+            account1.setBalance(account1.getBalance() + amount);
+            rsl = true;
+        }
+        return rsl;
+    }
+}

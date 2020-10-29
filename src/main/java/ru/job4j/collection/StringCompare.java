8322b8fe581dc66b8f9a10874d43c04d934e1429
anton <zas@see.ru>
2020-10-28 22:38:56 -0400

    Al

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -6,6 +6,26 @@ public class StringCompare implements Comparator<String> {
 
     @Override
     public int compare(String o1, String o2) {
-        return o1.compareTo(o2);
+        for (int i = 0; i < o1.length() && i < o2.length(); i++) {
+            if (o1.charAt(i) == o2.charAt(i)) {
+                continue;
+            } else {
+                return o1.charAt(i) - o2.charAt(i);
+            }
+        }
+        if (o1.length() != o2.length()) {
+            return o1.length() - o2.length();
+        } else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        String on = "";
+        String tw = "";
+        StringCompare stringCompare = new StringCompare();
+
+        System.out.println(stringCompare.compare(on, tw));
+
     }
 }

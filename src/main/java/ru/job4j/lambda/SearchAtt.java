7aaf2e44e872c78d0804026ba7945a48fdd6f473
anton <zas@see.ru>
2020-11-02 23:17:04 -0500

    Al

--- src/main/java/ru/job4j/lambda/SearchAtt.java
+++ src/main/java/ru/job4j/lambda/SearchAtt.java
@@ -1,6 +1,8 @@
 package ru.job4j.lambda;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;

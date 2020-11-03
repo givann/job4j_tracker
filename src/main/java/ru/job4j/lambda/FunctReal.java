ce05fbd6c6f79fbd738e8601343a077e1b1643fe
anton <zas@see.ru>
2020-11-03 02:35:49 -0500

    Al

--- src/main/java/ru/job4j/lambda/FunctReal.java
+++ src/main/java/ru/job4j/lambda/FunctReal.java
@@ -3,7 +3,9 @@ package ru.job4j.lambda;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class FunctReal {
     static List<Double> diapason(int start, int end, Function<Double, Double> func) {
@@ -12,5 +14,12 @@ public class FunctReal {
             l.add(func.apply((double) i));
         }
         return l;
+    }
+
+
+    public static <T, U> Function<T, U> ternaryOperator(Predicate<? super T> condition, Function<? super T,
+            ? extends U> ifTrue, Function<? super T, ? extends U> ifFalse) {
+        Function<T, U> nF = x -> condition.test(x) ? ifTrue.apply(x) : ifFalse.apply(x);
+        return nF;
     }
 }
--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<Person>();
@@ -10,13 +11,15 @@ public class PhoneDictionary {
     }
 
     public ArrayList<Person> find(String key) {
+        Predicate<Person> combine = x -> x.getName().contains(key) || x.getPhone().contains(key)
+                || x.getAddress().contains(key) || x.getSurname().contains(key);
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key) || person.getSurname().contains(key)
-                    || person.getAddress().contains(key) || person.getPhone().contains(key)) {
+            if (combine.test(person)) {
                 result.add(person);
             }
         }
         return result;
     }
 }
+

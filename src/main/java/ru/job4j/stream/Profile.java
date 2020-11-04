b7ce241bc5e2302b8866cc4dff6dac17f87b5fa7
anton <zas@see.ru>
2020-11-03 23:51:18 -0500

    Al

--- src/main/java/ru/job4j/stream/Profile.java
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -1,7 +1,9 @@
 package ru.job4j.stream;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Profile {
@@ -17,12 +19,34 @@ class Address {
     private String street;
     private int home;
     private int apartment;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Address address = (Address) o;
+        return home == address.home
+                && apartment == address.apartment
+                && Objects.equals(city, address.city)
+                && Objects.equals(street, address.street);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(city, street, home, apartment);
+    }
 }
 
 class Profiles {
     public List<Address> collect(List<Profile> profiles) {
         List<Address> addressList = profiles.stream()
                 .map(Profile::getAddress)
+                .sorted()
+                .distinct()
                 .collect(Collectors.toList());
         return addressList;
     }

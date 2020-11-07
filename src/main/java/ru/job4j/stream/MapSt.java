e175e9cba878ea5a288fabc57dc57b42459b4dad
anton <zas@see.ru>
2020-11-06 22:40:07 -0500

    MapSt

--- src/main/java/ru/job4j/stream/MapSt.java
+++ src/main/java/ru/job4j/stream/MapSt.java
@@ -9,7 +9,6 @@ import java.util.stream.Collectors;
 public class MapSt {
     static Map<String, Student> listToMap(List<Student> list) {
         Map<String, Student> mSt = list.stream()
-                .distinct()
                 .collect(Collectors.toMap(name -> name.getSurname(), student -> student,
                         (name, student) -> name));
         return mSt;
